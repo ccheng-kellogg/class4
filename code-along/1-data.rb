bc5d1ee2f74be2d759457017e26e87b6f7caf084
ccheng-kellogg Cheng <clarissa.cheng@kellogg.northwestern.edu>
2025-01-28 21:10:15 +0000

    ruby data conditionals arrays hashes

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,52 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 2
+puts 3
+
+puts 5 + 2
+puts 5 - 2
+puts 5 * 2
+puts 5 / 2.0
+
+puts (2 * 5) / 5
 
 # Non-numbers
 
+puts "hello, world!"
+puts "Tacos are" + " delish!"
+
+
 # True, False
+
+puts true
+puts false
 
 # Nothing
 
+puts nil 
+
 # Variables
+
+food = "tacos"
+quantity = 3
+
+puts food = quantity
 
 # Combine strings and variables
 
+puts "tacos: #(quantity)"
+
+puts "#(food): #(quantity)"
+
 # String manipulation
+
+puts "Hello".length
+puts "How are you today?" .capitalize
+puts "computer" .reverse
+
+creed = "This is the way"
+
+puts creed.upcase
+puts creed.downcase
+puts creed.swapcase
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -4,12 +4,56 @@
 
 # Booleans
 
+# test_is_true = true
+# puts test_is_true
+
+# test_is_false = false
+# puts test_is_false
+
 # Boolean Expressions
+
+# puts 3 == 2
+# puts 3 != 2
+# puts 3 > 2
+# puts 3 < 2
 
 # If Conditional Logic
 
+if 3 == 2
+    puts "This test should never display."
+end
+
+ if 3 > 2
+    puts "This text should always display."
+end
+
+
 # If/Else Conditional Logic
+
+if 3 == 2
+    puts "This test should never display."
+else
+    puts "This text should always display."
+end
 
 # Elsif Conditional Logic
 
+your_team_score = 2
+other_team_score = 2
+
+if your_team_score > other_team_score
+    puts "You won!"
+elsif your_team_score == other_team_score
+    puts "Tied"
+else
+    "Your team lost"
+end
+
 # Combining Expressions
+
+temp = 68
+precipitation = 0
+
+if temp >= 65 && temp <= 75 || precipitation == 0
+    puts "It's perfect outside."
+end
